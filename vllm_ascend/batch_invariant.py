a43a9a0fd3021dc3e110e003450540609acc022c
Lord_of_Ironhill <suiweiyi@huawei.com>
2025-12-09 15:10:38 +0800

    fix softmax bug

--- vllm_ascend/batch_invariant.py
+++ vllm_ascend/batch_invariant.py
@@ -500,7 +500,8 @@ def softmax_batch_invariant(input, dim, dtype=None):
     # Compute softmax in a deterministic way
     # First subtract max for numerical stability (standard practice)
     input_max = torch.amax(input, dim=dim, keepdim=True)
-    input = input - input_max    exp_x = torch.exp(input)
+    input = input - input_max    
+    exp_x = torch.exp(input)
     sum_exp_x = torch.sum(exp_x, dim=dim, keepdim=True)
     return exp_x / sum_exp_x
 
